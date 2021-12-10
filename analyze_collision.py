16c9a1b6202920131eb57267e8165b93a73f4e5a
Oscar Litorell <oscar.litorell@gmail.com>
2021-12-10 08:19:20 +0100

    Update analyze_collision.py

--- analyze_collision.py
+++ analyze_collision.py
@@ -3,9 +3,13 @@ import os
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.ndimage.interpolation import _prepad_for_spline_filter
 import scipy.signal as signal
+from scipy.signal.filter_design import normalize
+from scipy.signal.ltisys import impulse
 import results_analysis as ra
-
+import json
+import glob
 
 def filter(data, axis=0):
     return signal.savgol_filter(data, 51, 1, axis=axis)
@@ -14,13 +18,26 @@ def main(name):
     directory = f"analyzed_results/movement/{name}"
     objects = [f for f in os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
 
+    group = os.path.normpath(name).split(os.path.sep)[0]
+
+    constellations_path = os.path.join("analyzed_results", "constellations", group)
+
+    all_constellations = dict((os.path.split(file)[-1][:-5], file) for file in glob.iglob(f"{constellations_path}/**/*.json", recursive=True))
+
+
     collision_info = []
 
     for object in objects:
         pos = np.loadtxt(f"{directory}/{object}/position.tsv", delimiter="\t")
         rot = np.loadtxt(f"{directory}/{object}/rotation.tsv", delimiter="\t")
         rot = np.unwrap(rot)
-        dt = 0.01 # Needs a real input
+        
+        dt = np.loadtxt(f"{directory}/dt.txt", delimiter="\t")
+
+        with open(all_constellations[object]) as f:
+            object_info = json.load(f)
+            print(f)
+        print(object_info)
 
         pos_f = filter(pos, axis=0)
         vel = np.gradient(pos_f, axis=0) / dt
@@ -56,6 +73,9 @@ def main(name):
             "pos": collision_position,
             "vel": (vel_before_f[-1], vel_after_f[0]),
             "rot_vel": (rot_vel_before_f[-1], rot_vel_after_f[0]),
+            "mass": object_info["mass"],
+            "radius": object_info["radius"],
+            "height": object_info["height"]
         }
         # Eventuellt problem:
         # Svårt att veta exakta hastigheter precis före och efter
@@ -63,8 +83,88 @@ def main(name):
         # ordningens Savitsky-Golay-filter.
 
         collision_info.append(info)
+    
+    normal = collision_info[0]["pos"] - collision_info[1]["pos"]
+    normal = normal / np.linalg.norm(normal)
+    tangent = np.array([-normal[1], normal[0]])
+    rel_tangent_vel_before = np.dot(collision_info[0]["vel"][0] - collision_info[1]["vel"][0], tangent)
+    rel_tangent_vel_before += collision_info[0]["radius"] * collision_info[0]["rot_vel"][0]
+    rel_tangent_vel_before -= collision_info[1]["radius"] * collision_info[1]["rot_vel"][0]
+    rel_tangent_vel_after = np.dot(collision_info[0]["vel"][1] - collision_info[1]["vel"][1], tangent)
+    rel_tangent_vel_after += collision_info[0]["radius"] * collision_info[0]["rot_vel"][1]
+    rel_tangent_vel_after -= collision_info[1]["radius"] * collision_info[1]["rot_vel"][1]
+    print("Relative tangent velocity before:", rel_tangent_vel_before)
+    print("Relative tangent velocity after:", rel_tangent_vel_after)
+    rel_normal_vel_before = abs(np.dot(collision_info[0]["vel"][0] - collision_info[1]["vel"][0], normal))
+    rel_normal_vel_after = abs(np.dot(collision_info[0]["vel"][1] - collision_info[1]["vel"][1], normal))
 
-    print(collision_info)
+    momentum_0_before = collision_info[0]["mass"] * collision_info[0]["vel"][0]
+    momentum_1_before = collision_info[1]["mass"] * collision_info[1]["vel"][0]
+    momentum_0_after = collision_info[0]["mass"] * collision_info[0]["vel"][1]
+    momentum_1_after = collision_info[1]["mass"] * collision_info[1]["vel"][1]
+    momentum_before = momentum_0_before + momentum_1_before
+    momentum_after = momentum_0_after + momentum_1_after
+    print("Momentum before:", momentum_before)
+    print("Momentum after:", momentum_after)
+    impulse_0 = momentum_0_before - momentum_0_after
+    impulse_1 = momentum_1_before - momentum_1_after
+    impulse = (impulse_0 - impulse_1) / 2
+    normal_impulse = np.dot(impulse, normal)
+    tangent_impulse = np.dot(impulse, tangent)
+    print("Normal impulse:", normal_impulse)
+    print("Tangent impulse:", tangent_impulse)
+    fric_coeff = abs(tangent_impulse / normal_impulse)
+    print("Friction coefficient:", fric_coeff)
+
+    rel_normal_vel_before = np.dot(collision_info[0]["vel"][0] - collision_info[1]["vel"][0], normal)
+    rel_normal_vel_after = np.dot(collision_info[0]["vel"][1] - collision_info[1]["vel"][1], normal)
+    print("Relative normal velocity before:", rel_normal_vel_before)
+    print("Relative normal velocity after:", rel_normal_vel_after)
+
+    
+    e = abs(rel_normal_vel_after / rel_normal_vel_before)
+    print("Elasiticity coefficient:", e)
+
+    moment_of_inertia_0 = abs(tangent_impulse * collision_info[0]["radius"] / (collision_info[0]["rot_vel"][1] - collision_info[0]["rot_vel"][0]))
+    moment_of_inertia_1 = abs(tangent_impulse * collision_info[1]["radius"] / (collision_info[1]["rot_vel"][1] - collision_info[1]["rot_vel"][0]))
+
+    collision_properties = {
+        "name": name,
+        "pos_0": collision_info[0]["pos"].tolist(),
+        "pos_1": collision_info[1]["pos"].tolist(),
+        "vel_0_before": collision_info[0]["vel"][0].tolist(),
+        "vel_1_before": collision_info[1]["vel"][0].tolist(),
+        "vel_0_after": collision_info[0]["vel"][1].tolist(),
+        "vel_1_after": collision_info[1]["vel"][1].tolist(),
+        "rot_vel_0_before": collision_info[0]["rot_vel"][0],
+        "rot_vel_1_before": collision_info[1]["rot_vel"][0],
+        "rot_vel_0_after": collision_info[0]["rot_vel"][1],
+        "rot_vel_1_after": collision_info[1]["rot_vel"][1],
+        "mass_0": collision_info[0]["mass"],
+        "mass_1": collision_info[1]["mass"],
+        "radius_0": collision_info[0]["radius"],
+        "radius_1": collision_info[1]["radius"],
+        "height_0": collision_info[0]["height"],
+        "height_1": collision_info[1]["height"],
+        "rel_tangent_vel_before": rel_tangent_vel_before,
+        "rel_tangent_vel_after": rel_tangent_vel_after,
+        "rel_normal_vel_before": rel_normal_vel_before,
+        "rel_normal_vel_after": rel_normal_vel_after,
+        "normal_impulse": normal_impulse,
+        "tangent_impulse": tangent_impulse,
+        "fric_coeff": fric_coeff,
+        "e": e,
+        "moment_of_inertia_0": moment_of_inertia_0,
+        "moment_of_inertia_1": moment_of_inertia_1,
+    }
+
+    path = os.path.join("analyzed_collisions", os.path.split(name)[0])
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(f"analyzed_collisions/{name}.json", "w") as f:
+        json.dump(collision_properties, f)
+   
+
 
 
 if __name__ == '__main__':
