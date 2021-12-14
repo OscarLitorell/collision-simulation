e737e1e477bdb8ff0c1bb3f90081e99d4848f4d0
Oscar Litorell <oscar.litorell@gmail.com>
2021-12-14 15:32:39 +0100

    Added pause function for debugging

--- batch_analyze_2d.py
+++ batch_analyze_2d.py
@@ -4,23 +4,31 @@ import detect_marker_positions
 import calculate_movement
 import analyze_collision
 import compile_analyzed_collisions_2d
+from sys import argv
 
 import os
 
-def main():
+def wait(yes):
+    if yes:
+        input("Press enter to continue...")
+
+def main(step_through):
     print("Converting qualisys data...")
     convert_qualisys_2d.main()
     print("Detecting marker positions...")
+    wait(step_through)
     detect_marker_positions.main()
     collision_names = [p[0] for p in os.walk("results") if "collisions" in p[0].split(os.path.sep) and len(p[2]) > 0]
     collision_names = [name[len("results/"):] for name in collision_names]
     print("Calculating movement and analyzing collisions...")
+    wait(step_through)
     for c in collision_names:
         calculate_movement.main(c, False)
         analyze_collision.main(c)
     print("Compiling analyzed collisions...")
+    wait(step_through)
     compile_analyzed_collisions_2d.main()
     print("Done")
 
 if __name__ == '__main__':
-    main()
+    main("-s" in argv)
