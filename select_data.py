c0f6a5d1e5fb444bf64deab06a567f94be073048
Oscar Litorell <oscar.litorell@gmail.com>
2021-12-14 16:17:38 +0100

    Create select_data.py

---
+++ select_data.py
@@ -0,0 +1,57 @@
+
+
+import numpy as np
+
+
+
+def main():
+    with open("analyzed_collisions.tsv", "r") as f:
+        lines = f.readlines()
+    lines = [line.strip().split("\t") for line in lines]
+    header = lines[0]
+    lines = lines[1:]
+
+    rows = np.array(lines)
+    cols = rows.T.tolist()
+
+
+    name = np.array(cols[header.index("name")])
+    pos_0_x = np.array(cols[header.index("pos_0_x")]).astype(float)
+    pos_0_y = np.array(cols[header.index("pos_0_y")]).astype(float)
+    vel_0_before_x = np.array(cols[header.index("vel_0_before_x")]).astype(float)
+    vel_0_before_y = np.array(cols[header.index("vel_0_before_y")]).astype(float)
+    vel_1_before_x = np.array(cols[header.index("vel_1_before_x")]).astype(float)
+    vel_1_before_y = np.array(cols[header.index("vel_1_before_y")]).astype(float)
+    vel_0_after_x = np.array(cols[header.index("vel_0_after_x")]).astype(float)
+    vel_0_after_y = np.array(cols[header.index("vel_0_after_y")]).astype(float)
+    vel_1_after_x = np.array(cols[header.index("vel_1_after_x")]).astype(float)
+    vel_1_after_y = np.array(cols[header.index("vel_1_after_y")]).astype(float)
+    rot_vel_0_before = np.array(cols[header.index("rot_vel_0_before")]).astype(float)
+    rot_vel_1_before = np.array(cols[header.index("rot_vel_1_before")]).astype(float)
+    rot_vel_0_after = np.array(cols[header.index("rot_vel_0_after")]).astype(float)
+    rot_vel_1_after = np.array(cols[header.index("rot_vel_1_after")]).astype(float)
+    mass_0 = np.array(cols[header.index("mass_0")]).astype(float)
+    mass_1 = np.array(cols[header.index("mass_1")]).astype(float)
+    radius_0 = np.array(cols[header.index("radius_0")]).astype(float)
+    radius_1 = np.array(cols[header.index("radius_1")]).astype(float)
+    height_0 = np.array(cols[header.index("height_0")]).astype(float)
+    height_1 = np.array(cols[header.index("height_1")]).astype(float)
+    rel_tangent_vel_before = np.array(cols[header.index("rel_tangent_vel_before")]).astype(float)
+    rel_tangent_vel_after = np.array(cols[header.index("rel_tangent_vel_after")]).astype(float)
+    rel_normal_vel_before = np.array(cols[header.index("rel_normal_vel_before")]).astype(float)
+    rel_normal_vel_after = np.array(cols[header.index("rel_normal_vel_after")]).astype(float)
+    normal_impulse = np.array(cols[header.index("normal_impulse")]).astype(float)
+    tangent_impulse = np.array(cols[header.index("tangent_impulse")]).astype(float)
+    fric_coeff = np.array(cols[header.index("fric_coeff")]).astype(float)
+    e = np.array(cols[header.index("e")]).astype(float)
+    moment_of_inertia_0 = np.array(cols[header.index("moment_of_inertia_0")]).astype(float)
+    moment_of_inertia_1 = np.array(cols[header.index("moment_of_inertia_1")]).astype(float)
+        
+
+    input("Press enter to continue...")
+
+
+
+
+if __name__ == '__main__':
+    main()
