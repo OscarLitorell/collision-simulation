c064e51902f3ea4201b68fbe5f1e368dcd27d2f3
Oscar Litorell <oscar.litorell@gmail.com>
2021-12-18 21:41:20 +0100

    Update select_data.py

--- select_data.py
+++ select_data.py
@@ -47,6 +47,14 @@ def main():
     e = np.array(cols[header.index("e")]).astype(float)
     moment_of_inertia_0 = np.array(cols[header.index("moment_of_inertia_0")]).astype(float)
     moment_of_inertia_1 = np.array(cols[header.index("moment_of_inertia_1")]).astype(float)
+
+    total_momentum_before_x = mass_0 * vel_0_before_x + mass_1 * vel_1_before_x
+    total_momentum_before_y = mass_0 * vel_0_before_y + mass_1 * vel_1_before_y
+    total_momentum_after_x = mass_0 * vel_0_after_x + mass_1 * vel_1_after_x
+    total_momentum_after_y = mass_0 * vel_0_after_y + mass_1 * vel_1_after_y
+    momentum_diff_x = total_momentum_after_x - total_momentum_before_x
+    momentum_diff_y = total_momentum_after_y - total_momentum_before_y
+    momentum_diff = np.sqrt(momentum_diff_x**2 + momentum_diff_y**2)
         
     group = [os.path.normpath(n).split(os.path.sep)[0] for n in name]
     groups = list(set(group))
@@ -62,7 +70,34 @@ def main():
         plt.plot(x, y, "o")
         plt.show()
         
+    blacklist = [
+        "aluBig-alu\\collisions\\T_aluBig_Y-alu_spin0031",
+        "aluBig-aluThi\\collisions\\Y_aluThi_T-aluBig_5cm0013",
+        "aluBig-aluThi\\collisions\\Y_aluThi_T-aluBig_5cm0016",
+        "aluRub-aluBig\\collisions\\T_aluBig_Y-aluRub0020",
+        "aluRub-aluThi\\collisions\\Y_aluThi_T-aluRub0011",
+        "aluTunn-alu\\collisions\\Y_aluTunn_Y_alu0003",
+        "alu-aluRub\\collisions\\T-alu_Y-aluRub_2cm0017"
+    ]
 
+    sel = np.array([n not in blacklist for n in name])
+
+
+    fig, ax = plt.subplots()
+    ax.scatter(x=group[sel], y=momentum_diff[sel])
+    plt.show()
+
+
+
+    for g in groups:
+        print(g)
+        mask = np.all([group==g, sel], axis=0)
+        x = abs(normal_impulse[mask])
+        y = e[mask]
+        plt.plot(x, y, "o")
+        plt.title(g)
+        plt.show()
+        
 
     input("Press enter to continue...")
 
